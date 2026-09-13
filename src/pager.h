0e5ee4d19c4a0c485e95ed639153ad1d9d5ae525
Puneet <dalalpuneet400@gmail.com>
2026-09-14 00:56:22 +0530

    pager hahahahahhhahahhaahhahahahahahhahhah

---
+++ src/pager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <cstdint>
+#include <string> 
+
+class pager{
+    private:
+        std::fstream dbfile;
+        std:: unordered_map <uint32_t,uint8_t*> page_cache;
+    public:
+        pager(const std::string& filename);
+        ~pager();
+        uint8_t get_page(uint32_t page_num);
+        void flsh_page(uint32_t page_num);
+        uint32_t allocate_page();
+
+};
